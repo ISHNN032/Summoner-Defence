1d89300a7b5353600d7ce9d13c80593f303a795e
Unknown <ishnn032n@gmail.com>
2018-05-23 23:43:37 +0900

    Unit Refactored : Unfin;
    유닛 리펙토링 중

--- Assets/Resources/Scripts/UnitSystem/Unit.cs
+++ Assets/Resources/Scripts/UnitSystem/Unit.cs
@@ -13,13 +13,17 @@ public abstract class Unit : MonoBehaviour
     protected Direction look_direction;
     protected Mode mode;
 
+    [SerializeField] protected float find_distance = 2f;
+
     [SerializeField] protected float disableTime = 1;
     protected WaitForSeconds wait;
 
     protected Rigidbody2D _rigidbody;
     protected BoxCollider2D _collider;
+    protected LayerMask _layerMask;
 
     [SerializeField] GameObject hitbox;
+    [SerializeField] LayerMask enemy_layer;
 
     protected virtual void Awake()
     {
@@ -28,33 +32,24 @@ public abstract class Unit : MonoBehaviour
 
         wait = new WaitForSeconds(disableTime);
     }
+
     protected virtual void Start()
     {
+        Debug.Log(_layerMask);
+
         mode = Mode.Capture;
         StartCoroutine("CheckOverlap");
-        StartCoroutine("FindEnemy");
-        //hitbox를 따로 두는 것 대신 오버랩 박스를 이용
-    }
-    protected virtual void OnTriggerEnter2D(Collider2D collision)
-    {
-        //_rigidbody.AddForce(Vector3.right + Vector3.up * 10, ForceMode2D.Impulse);
-        Debug.Log(string.Format("{0} : {1}", this.name, collision.name));
-    }
-
-    protected virtual void Attack()
-    {
-        GameObject attack = GameObject.Instantiate(hitbox,this.transform);
-        attack.transform.Translate((float)look_direction * 0.5f,0,0);
+        StartCoroutine("FindCoroutine");
     }
 
     protected void Move(Direction direct)
     {
-        StopCoroutine("EscortCoroutine");
-        StopCoroutine("Combat");
-        StopCoroutine("Chase");
-
         StopCoroutine("MoveCoroutine");
+        StopCoroutine("CombatCoroutine");
+        StopCoroutine("EscortCoroutine");
+        
         direction = direct;
+        look_direction = direct;
         StartCoroutine("MoveCoroutine");
     }
 
@@ -62,8 +57,6 @@ public abstract class Unit : MonoBehaviour
     {
         StopCoroutine("MoveCoroutine");
         StopCoroutine("EscortCoroutine");
-        StopCoroutine("Combat");
-        StopCoroutine("Chase");
     }
 
     protected void Capture()
@@ -81,55 +74,96 @@ public abstract class Unit : MonoBehaviour
         StartCoroutine("EFindEnemy");
     }
 
-    //*/코루틴에서 실행해주는 내부적인 기능함수>>
-
-    private void DirectionToPlayer()
-    {
-        Debug.DrawLine(transform.position, (Vector2)transform.position + Vector2.up, Color.yellow);
-        float distance = this.transform.position.x - PlayerController.Instance.transform.position.x;
-
-        if (distance > 2)
-        {
-            direction = Direction.Left;
-        }
-        else if (distance < -2)
-        {
-            direction = Direction.Right;
-        }
-        else if (distance <= 2 && distance > 0)
-        {
-            look_direction = Direction.Right;
-            StopCoroutine("MoveCoroutine");
-        }
-        else if (distance >= -2 && distance < 0)
-        {
-            look_direction = Direction.Left;
-            StopCoroutine("MoveCoroutine");
-        }
-    }
-
-    //*/코루틴에서 실행해주는 내부적인 기능함수<<
-
-
-    protected IEnumerator EscortCoroutine()
-    {
-        while (true)
-        {
-            DirectionToPlayer();
-            yield return null;
-        }
-    }
-
     protected IEnumerator MoveCoroutine()
     {
         while (true)
         {
-            if (Time.timeScale == 0) {
+            if (Time.timeScale == 0)
+            {
                 yield return null;
                 continue;
             }
             this.transform.Translate(Vector2.Lerp(Vector2.zero, new Vector2((float)direction * speed, 0), 0.1f));
-            look_direction = direction;
+            //look_direction = direction;
+            yield return null;
+        }
+    }
+
+    protected IEnumerator FindCoroutine()
+    {
+        while (true)
+        {
+            if (Time.timeScale == 0)
+            {
+                yield return null;
+                continue;
+            }
+
+            Debug.DrawRay(transform.forward, new Vector2((float)look_direction, 0) * find_distance, Color.cyan);
+            var hit = Physics2D.Raycast(transform.position, new Vector2((float)look_direction,0) , find_distance, 1 << enemy_layer);
+            if (hit && !this.tag.Equals(hit.transform.tag))
+            {
+                Debug.Log(string.Format("{0} Found The Enemy {1}", this.name, hit.transform.name));
+                //StartCoroutine("Combat", hit.transform.gameObject);
+                yield return wait;
+            }
+            yield return null;
+        }
+    }
+    
+    protected IEnumerator CombatCoroutine(GameObject enemy)
+    {
+        float distance;
+        while (enemy != null)
+        {
+            //적과 일정 거리 이상 가까워지면 멈춘 후 공격
+            distance = this.transform.position.x - enemy.transform.position.x;
+
+            if (Mathf.Abs(distance) <= 1.2)
+            {
+                direction = 0;
+                if(distance > 0)
+                {
+                    look_direction = Direction.Left;
+                }
+                else
+                {
+                    look_direction = Direction.Right;
+                }
+                GameObject attack = GameObject.Instantiate(hitbox, this.transform);
+                attack.transform.Translate((float)look_direction * 0.5f, 0, 0);
+            }
+            yield return new WaitForSeconds(1);
+        }
+    }
+    
+    protected IEnumerator EscortCoroutine()
+    {
+        while (true)
+        {
+            //플레이어에게 일정 거리를 두고 따라온다.
+            Debug.DrawLine(transform.position, (Vector2)transform.position + Vector2.up, Color.yellow);
+            float distance = this.transform.position.x - PlayerController.Instance.transform.position.x;
+
+            if (distance > 2)
+            {
+                direction = Direction.Left;
+            }
+            else if (distance < -2)
+            {
+                direction = Direction.Right;
+            }
+            else if (distance <= 2 && distance > 0)
+            {
+                look_direction = Direction.Right;
+                direction = 0;
+            }
+            else if (distance >= -2 && distance < 0)
+            {
+                look_direction = Direction.Left;
+                direction = 0;
+            }
+            
             yield return null;
         }
     }
@@ -145,118 +179,17 @@ public abstract class Unit : MonoBehaviour
                 continue;
             }
 
-            var overlap = Physics2D.OverlapBox(transform.position, _collider.size, 0, 1 << 10);
+            var overlap = Physics2D.OverlapBox(transform.position, _collider.size, 0, 1 << 11);
 
             //오버랩이 존재하며, 본인과 다른 태그의 히트박스일때
             if (overlap != null && !this.tag.Equals(overlap.tag))
             {
                 float distance = this.transform.position.x - overlap.transform.parent.position.x;
 
-                Debug.Log(string.Format("{0} :: {1} - {2}", this.name, overlap.name));
-                _rigidbody.AddForce(new Vector2(distance * 3,0), ForceMode2D.Impulse);
+                Debug.Log(string.Format("{0} :: {1}", this.name, overlap.name));
+                _rigidbody.AddForce(new Vector2(distance * 3, 0), ForceMode2D.Impulse);
                 yield return wait;
                 //레이어10 (hitbox)에 해당하는 것이 충돌되었을때 지정된 초 동안 검사중지
-            }
-            yield return null;
-        }
-    }
-    
-    //적을 찾는 코루틴이다
-    protected IEnumerator FindEnemy()
-    {
-        while (true)
-        {
-            if (Time.timeScale == 0)
-            {
-                yield return null;
-                continue;
-            }
-
-            Debug.DrawLine(transform.position, (Vector2)transform.position + new Vector2((float)look_direction, 0) * 2, Color.cyan);
-            var hits = Physics2D.RaycastAll(transform.position, new Vector2((float)look_direction, 0), 2, 1 << 8);
-            for(int i=0; i< hits.Length; ++i)
-            {
-                var hit = hits[i];
-                if (hit && !this.tag.Equals(hit.transform.tag))
-                {
-                    Debug.Log(string.Format("{0} Found The Enemy {1}", this.name, hit.transform.name));
-                    StartCoroutine("Combat",hit.transform.gameObject);
-                    StartCoroutine("Chase", hit.transform.gameObject);
-                    yield return wait;
-                }
-            }
-            yield return null;
-        }
-    }
-
-    protected IEnumerator EFindEnemy()
-    {
-        while (true)
-        {
-            if (Time.timeScale == 0)
-            {
-                yield return null;
-                continue;
-            }
-
-            Debug.DrawLine((Vector2)transform.position + Vector2.left, (Vector2)transform.position + Vector2.right * 2, Color.cyan);
-
-            var hits = Physics2D.RaycastAll((Vector2)transform.position - Vector2.left * 2f , Vector2.right * 2f, 4, 1 << 8);
-            for (int i = 0; i < hits.Length; ++i)
-            {
-                var hit = hits[i];
-                if (hit && !this.tag.Equals(hit.transform.tag))
-                {
-                    Debug.Log(string.Format("{0} Found The Enemy {1}", this.name, hit.transform.name));
-                    StartCoroutine("Combat", hit.transform.gameObject);
-                    yield return wait;
-                }
-            }
-            yield return null;
-        }
-    }
-
-    protected IEnumerator Combat(GameObject enemy)
-    {
-        while (enemy != null)
-        {
-            Attack();
-            yield return new WaitForSeconds(1);
-        }
-    }
-
-    protected IEnumerator Chase(GameObject enemy)
-    {
-        StopCoroutine("FindEnemy");
-        while (enemy != null)
-        {
-            if (Time.timeScale == 0)
-            {
-                yield return null;
-                continue;
-            }
-
-            float distance = this.transform.position.x - enemy.transform.position.x;
-
-            Debug.DrawLine(transform.position, (Vector2)transform.position + Vector2.up + Vector2.left, Color.red);
-
-            if (distance > 1.2f)
-            {
-                direction = Direction.Left;
-            }
-            else if (distance < -1.2f)
-            {
-                direction = Direction.Right;
-            }
-            else if (distance <= 1.2 && distance > 0)
-            {
-                look_direction = Direction.Left;
-                StopCoroutine("MoveCoroutine");
-            }
-            else if (distance >= -1.2 && distance < 0)
-            {
-                look_direction = Direction.Right;
-                StopCoroutine("MoveCoroutine");
             }
             yield return null;
         }
